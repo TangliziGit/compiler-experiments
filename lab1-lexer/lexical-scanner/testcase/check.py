f04af6852fb1f6ac6bdcebc0c86dde412c7a47e9
TangliziGit <tanglizimail@foxmail.com>
2020-04-06 00:29:02 +0800

    add a simple script to check testcase

---
+++ lab1-lexer/lexical-scanner/testcase/check.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+files = os.listdir('.')
+codes = filter(lambda x: x.endswith('.txt'), files)
+
+for code in codes:
+    print(code)
+    out = open(code + '.out', 'r').read()
+    code = open(code, 'r').read()
+
+    code = ''.join(re.sub('[ \t\n]', '', code))
+    out = [x.split(', ')[1] for x in out.split('\n')]
+    out = ''.join(out)
+
+    print(code == out)
+    print(code)
+    print(out)
+    print()
